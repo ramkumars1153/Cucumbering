583f87f5e1563730dab555d18239f31c0f710e6e
ramku <ramku@DESKTOP-A8AF0PQ>
2021-05-23 19:00:13 +0530

    bhhhbb

--- Cucumbering/src/test/java/Excel/Cucumbering/CucumberClass.java
+++ Cucumbering/src/test/java/Excel/Cucumbering/CucumberClass.java
@@ -1,10 +1,23 @@
 package Excel.Cucumbering;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class CucumberClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("New class is created by RAM");
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.navigate().refresh();
+		
+		Boolean isenable = driver.findElement(By.xpath("")).isEnabled();
+		System.out.println(isenable);
+		
 	}
 
 }
