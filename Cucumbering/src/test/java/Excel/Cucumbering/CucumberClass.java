a4b21887ce50b150cc51b9379a88fbe0bf5ffd19
ramkumars1153 <77505586+ramkumars1153@users.noreply.github.com>
2021-05-23 18:08:07 +0530

    pusin new shcnag

--- Cucumbering/src/test/java/Excel/Cucumbering/CucumberClass.java
+++ Cucumbering/src/test/java/Excel/Cucumbering/CucumberClass.java
@@ -6,6 +6,8 @@ public class CucumberClass {
 		// TODO Auto-generated method stub
 		System.out.print("New class is created by RAM");
 		System.out.print("New line is created via GitHub Changing line in Eclispe");
+		System.out.print("New line to understandmore");
+
 	}
 
 }
