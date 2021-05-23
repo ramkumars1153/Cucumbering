b3c1eedb86b0dee310fcb841fa63f2f5f9836f03
ramkumars1153 <77505586+ramkumars1153@users.noreply.github.com>
2021-05-23 17:58:27 +0530

    Changes are made

--- Cucumbering/src/test/java/Excel/Cucumbering/CucumberClass.java
+++ Cucumbering/src/test/java/Excel/Cucumbering/CucumberClass.java
@@ -5,7 +5,7 @@ public class CucumberClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("New class is created by RAM");
-		System.out.print("New line is created via GitHub");
+		System.out.print("New line is created via GitHub changing same line in GitHub too");
 	}
 
 }
