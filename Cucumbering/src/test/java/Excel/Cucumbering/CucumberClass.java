ddd8e01467a106f9485dd9def8da3f5c9e2deb8b
ramku <ramku@DESKTOP-A8AF0PQ>
2021-05-23 18:11:51 +0530

    commit

--- Cucumbering/src/test/java/Excel/Cucumbering/CucumberClass.java
+++ Cucumbering/src/test/java/Excel/Cucumbering/CucumberClass.java
@@ -5,12 +5,8 @@ public class CucumberClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("New class is created by RAM");
-		System.out.print("New line is created via GitHub Changing line in Eclispe");		
-<<<<<<< HEAD
-		System.out.print("New Changes and line sdadsfsdf");
-=======
+		System.out.print("New line is created via GitHub Changing line in Eclispe");
 		System.out.print("New Changes and line changes in new");
->>>>>>> branch 'Branching_Learning' of https://github.com/ramkumars1153/Cucumbering.git
 		System.out.print("New line to understandmore");
 
 	}
