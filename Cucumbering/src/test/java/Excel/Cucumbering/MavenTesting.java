b6d80eeea7294a1370070921a85903023e1824b8
ramku <ramku@DESKTOP-A8AF0PQ>
2021-05-23 19:01:36 +0530

    hhhb

--- Cucumbering/src/test/java/Excel/Cucumbering/MavenTesting.java
+++ Cucumbering/src/test/java/Excel/Cucumbering/MavenTesting.java
@@ -10,12 +10,8 @@ public class MavenTesting {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-<<<<<<< HEAD
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().setSize(new Dimension(600, 800));
-=======
-		
->>>>>>> refs/heads/main
 	}
 }
