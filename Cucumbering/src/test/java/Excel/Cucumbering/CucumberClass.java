e05a725f92d6df0078bb95cd1f1fddf3737d9146
ramkumars1153 <77505586+ramkumars1153@users.noreply.github.com>
2021-05-23 18:00:49 +0530

    Changes in already existing line

--- Cucumbering/src/test/java/Excel/Cucumbering/CucumberClass.java
+++ Cucumbering/src/test/java/Excel/Cucumbering/CucumberClass.java
@@ -4,7 +4,7 @@ public class CucumberClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("New class is created by RAM");
+		System.out.print("New class is created by RAM edited in GitHub");
 		System.out.print("New line is created via GitHub changing same line in GitHub too");
 	}
 
