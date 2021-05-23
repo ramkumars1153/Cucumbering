6db832a9207ef8fe1f532b42393542f506a77707
ramku <ramku@DESKTOP-A8AF0PQ>
2021-05-23 18:12:22 +0530

    Delete

--- Cucumbering/src/test/java/Excel/Cucumbering/CucumberClass.java
+++ Cucumbering/src/test/java/Excel/Cucumbering/CucumberClass.java
@@ -5,9 +5,6 @@ public class CucumberClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("New class is created by RAM");
-		System.out.print("New line is created via GitHub Changing line in Eclispe");
-		System.out.print("New Changes and line changes in new");
-		System.out.print("New line to understandmore");
 
 	}
 
